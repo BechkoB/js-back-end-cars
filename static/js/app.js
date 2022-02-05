5fb010e3d3a1337f0ee9cd451ead047a6aee604b
Behchet <bechko.b@gmail.com>
2022-02-05 23:54:54 +0200

    Implemented car listing, added front end code

---
+++ static/js/app.js
@@ -0,0 +1,14 @@
+document.getElementById('cars').addEventListener('click', (ev) => {
+    if (ev.target.classList.contains('more')) {
+        const desc = ev.target.parentElement.querySelector('.description');
+
+        if (desc.style.display == 'block') {
+            desc.style.display = '';
+            ev.target.textContent = 'Show More';
+        } else {
+            desc.style.display = 'block';
+            ev.target.textContent = 'Hide';
+
+        }
+    }
+})
